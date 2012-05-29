0580b191cc09ff13422b2bd9982825e36122d82a
h42 <jdinardo@nycap.rr.com>
2012-05-28 21:28:22 -0400

    played with p04 for speed

--- p04.hs
+++ p04.hs
@@ -1,27 +1,15 @@
+checker x = sx == reverse sx where sx = show x
+pal = maximum [x*y | x<-[100..999], y<-[100..999], checker (x*y)] :: Int
 
-checker x = ans2 where
-    (q1,r1) = quotRem x 10
-    (q2,r2) = quotRem q1 10
-    (q3,r3) = quotRem q2 10
-    (q4,r4) = quotRem q3 10
-    (q5,r5) = quotRem q4 10
-    (q6,r6) = quotRem q5 10
-    ans2 = if r1 == r6 && r2 == r5 && r3 == r4 then True else False
+ns' = [x*y | x<-[100..999], y<-[100..999], rem (x*y) 11 == 0] :: [Int]
+ploop [] a = a
+ploop (n:ns) a = ploop ns (if n>a && checker n then n else a)
 
-ul = 999
-xy :: [(Int,Int)]
-xy = do
-    x <- [100..ul]
-    y <- [100..ul]
-    return (x,y)
+-- second solution is much faster but rem 11 only works for 6 digit product
 
-f1 (ax,ay) (vx,vy) = ans  where
-    an = ax * ay
-    vn = vx * vy
-    ans = if vn > an && checker vn then (vx,vy) else (ax,ay)
+main = print $ ploop ns' 0
 
-main = do
-    let (x,y) = foldl f1 (0,0) xy
-    print (x*y)
-    print (x,y)
+
+
+
 
