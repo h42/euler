9d59b5428bbc834764eef658db859bf2aadba79d
h42 <jdinardo@nycap.rr.com>
2012-06-05 20:04:05 -0400

    changed p^2 to p*p for speedup in p07.hs

--- p07.hs
+++ p07.hs
@@ -1,10 +1,9 @@
-
-primes :: [Integer]
+primes :: [Int]
 primes = 2 : 3 : [x | x<- [5,7..], isPrime primes x ]
 isPrime (p:ps) x
-    | p^2 > x   = True
+    | p*p > x   = True
     | otherwise = if rem x p == 0 then False else isPrime ps x
 
 main = do
-    print $ head $ drop 10000 primes
+    print $ head $ drop 100000 primes
 
