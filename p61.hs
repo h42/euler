21efbd1d67a7c3cc912fb359bb69df50d2e75468
h42 <jdinardo@nycap.rr.com>
2012-06-19 09:22:03 -0400

    finished p61

--- p61.hs
+++ p61.hs
@@ -1,31 +1,31 @@
 import Data.Char
-main = do
-    print $ length s3
-    print $ length s4
-    print $ length s5
-    print $ length s6
-    let ans =  solve s3 s4 s5 s6
-    mapM print ans
+import Data.List
 
-s3 = fsum $ takeWhile (<10000) $ dropWhile (<1000) $ map (\n->quot (n*(n+1)) 2) [2..]
-s4 = fsum $ takeWhile (<10000) $ dropWhile (<1000) $ map (\n->n*n) [2..]
-s5 = fsum $ takeWhile (<10000) $ dropWhile (<1000) $ map (\n->quot (n*(3*n-1)) 2 ) [2..]
-s6 = fsum $ takeWhile (<10000) $ dropWhile (<1000) $ map (\n->n*(2*n-1)) [2..]
-s7 = fsum $ takeWhile (<10000) $ dropWhile (<1000) $ map (\n->quot (n*(5*n-3)) 2) [2..]
-s8 = fsum $ takeWhile (<10000) $ dropWhile (<1000) $ map (\n->n*(3*n-2)) [2..]
+main = print $ filter (not.null)
+	    $ map solve6 $ map ([s3]++) (permutations [s4,s5,s6,s7,s8])
 
-conv x = (q,r,x) where (q,r) = quotRem x 100
-fsum sl = map conv sl
+s3 = getvals $ map (\n->quot (n*(n+1)) 2) [2..]
+s4 = getvals $ map (\n->n*n) [2..]
+s5 = getvals $ map (\n->quot (n*(3*n-1)) 2 ) [2..]
+s6 = getvals $ map (\n->n*(2*n-1)) [2..]
+s7 = getvals $ map (\n->quot (n*(5*n-3)) 2) [2..]
+s8 = getvals $ map (\n->n*(3*n-2)) [2..]
 
-solve l1 l2 l3 l4 = do
+getvals xs = takeWhile (<10000) $ dropWhile (<1000) xs
+
+solve6 [l1,l2,l3,l4,l5,l6] = do
     a<-l1
     let m = comp a l2
     b<-m
     let n = comp b l3
     c<-n
-    let o = comp2 a c l4
+    let o = comp c l4
     d<-o
-    return (a,b,c,d)
+    let p = comp d l5
+    e<-p
+    let q = comp2 a e l6
+    f<-q
+    return (a + b + c + d + e + f)
 
-comp (xl,xr,xn) = filter (\(l,r,n) -> xr == l)
-comp2 (al,ar,an) (xl,xr,xn) = filter (\(l,r,n) -> xr == l && al == r)
+comp x = filter (\l -> rem x 100 == quot l 100)
+comp2 a x = filter (\l -> rem x 100 == quot l 100  && quot a 100 == rem l 100)
