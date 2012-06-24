f16e6e616798b34f97e5fd11e7c596234e5b65e1
h42 <jdinardo@nycap.rr.com>
2012-06-24 08:52:11 -0400

    finished 65

--- p64.hs
+++ p64.hs
@@ -1,25 +1,22 @@
-import Data.Ratio
-import Debug.Trace
+maxn = 10000
+maxcnt = maxn + 1
 
 step1 :: Int -> [Int]
 step1 n = ans where
     rn = floor $ sqrt $ fromIntegral n
     ans = if rn ^ 2 == n  then  [rn]
-	  else step2 n rn (0) (1) [rn] 15
+	  else step2 n rn (0) (1) [rn] maxcnt (-1,-1,-1)
 
-step2 _ _ _ _ ans 0 = reverse ans
-step2 n rn c d ans cnt  = step2 n rn c' d' ans' (cnt-1) where
+step2 _ _ _ _ ans 0 _ = reverse $ if length ans == 1 then ans else tail ans
+step2 n rn c d ans cnt (ca,cc,cd) = step2 n rn c' d' ans' cnt' (ca',cc',cd')
+  where
     a = head ans
     c' = - (c - a * d)
     d' = (n - c'^2) `quot` d
     a' = d * (rn  + c') `quot` (n - c'^2)
     ans' = a' : ans
+    (ca',cc',cd') = if ca<0 then (a',c',d') else (ca,cc,cd)
+    cnt' =  if a' == ca && c' == cc && d' == cd  then 0  else cnt - 1
 
 main = do
-    print $ step1 2
-    print $ step1 3
-    print $ step1 5
-    print $ step1 6
-    print $ step1 7
-    print $ step1 23
-    print $ step1 46
+    print $ length $ filter even $ map (length.step1) [2..maxn]
