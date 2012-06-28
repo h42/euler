9209f5c3b3f49d426d50748ca3b1b5eb11151bf6
h42 <jdinardo@nycap.rr.com>
2012-06-28 15:13:09 -0400

    finished 68

--- p68.hs
+++ p68.hs
@@ -1,6 +1,7 @@
 import Data.List
+import Data.Maybe
 
-ml = permutations [1..10]
+-- magig 5gon
 
 fl = filter ff $ permutations [1..10] where
     ff [a,b,c,d,e,f,g,h,i,j] =
@@ -9,11 +10,16 @@ fl = filter ff $ permutations [1..10] where
 	a+b+c == h+g+i &&
 	a+b+c == j+i+b
 
-tl = map tf fl where
+tl =  map disp (map tf fl) where
     tf [a,b,c,d,e,f,g,h,i,j] = [(a,b,c),(d,c,e),(f,e,g),(h,g,i),(j,i,b)]
 
-main = do
-    print $ length ml
-    print $ length fl
-    mapM print $ take 10 tl
+disp xs = showl ys where
+    ei = minimum xs
+    i = fromJust $ elemIndex ei xs
+    ys = (take (5-i) $ drop i xs) ++ take i xs
 
+    showl [x1,x2,x3,x4,x5] = show2 x1 ++ show2 x2 ++ show2 x3
+				      ++ show2 x4 ++ show2 x5
+	where show2 (a,b,c) = show a ++ show b ++ show c
+
+main = print $ last $ nub $ sort $ filter (\x->length x == 16) tl
