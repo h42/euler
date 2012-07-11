453c5cbb786a37997af8299303897218d90b043d
h42 <jdinardo@nycap.rr.com>
2012-07-10 21:25:30 -0400

    finished 81

--- p81.hs
+++ p81.hs
@@ -1,5 +1,16 @@
+as = fmap (\s-> map (\x->read ("["++x++"]")) $ lines s) $readFile "p81.txt"
 
+cost2 [] _ psum os = reverse os
+cost2 (x:xs) (x1:x1s) psum os = cost2 xs x1s c (c:os) where
+    c = min (x+x1) (x+psum)
 
-main = do
-    print $ 42
+cost1 [] os = os
+cost1 (y:ys) os = cost1 ys os' where
+    os' = cost2 y os 999999999 []
 
+cost :: [[Int]] -> Int
+cost (y:ys) = ans where
+    os = scanl1 (+) y
+    ans = last $ cost1 ys os
+
+main = fmap cost as >>= print
